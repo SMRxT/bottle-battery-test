145ebf5abc532b18302de6ded2069fc445fe3d7a
Stephen Reddekopp <stephen.reddekopp@gmail.com>
2023-07-17 10:28:57 -0500

    lookup the plug by mac

--- loop.ts
+++ loop.ts
@@ -1,35 +1,42 @@
-import { Client } from "tplink-smarthome-api";
+const tplink = require("tplink-smarthome-api");
 
-const client = new Client();
-
-// client.startDiscovery().on('plug-new', (plug) => {
-//   console.log('found one', plug)
-//   plug.getInfo().then(console.log);
-//   plug.getSysInfo().then(console.log);
-//   //plug.setPowerStte(true);
-// });
-
-// '70:4F:57:B4:4F:AB
-  const plug = client.getPlug({ host: "192.168.1.113" });
-
-
-console.log("cycling forever starting now", new Date().toISOString());
-
-const delay = async time => {
-  return new Promise(resolve => {
-    setTimeout(resolve, time);
-  });
-};
-
-const TIME_ON_TO_TRIGGER = 5 * 1000;
-const TIME_BETWEEN_TRIGGERS = 3 * 60 * 60 * 1000;
+const client = new tplink.Client();
 
 (async () => {
-  for (;;) {
-    console.log("cycling again", new Date().toISOString());
-    plug.setPowerState(true);
-    await delay(TIME_ON_TO_TRIGGER);
-    plug.setPowerState(false);
-    await delay(TIME_BETWEEN_TRIGGERS);
-  }
+  const host = await new Promise((callback) => {
+    client.startDiscovery().on("plug-new", (plug) => {
+      // plug.getInfo().then((info) => console.log("info", info));
+      plug.getSysInfo().then((info) => {
+        if (info.mac === "70:4F:57:B4:4F:AB") {
+          // console.log("host", plug.host);
+          // console.log("sys info", info);
+          callback(plug.host);
+        }
+      });
+    });
+  });
+
+  // '70:4F:57:B4:4F:AB
+  const plug = client.getPlug({ host: host });
+
+  console.log("cycling forever starting now", new Date().toISOString());
+
+  const delay = async (time) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, time);
+    });
+  };
+
+  const TIME_ON_TO_TRIGGER = 5 * 1000;
+  const TIME_BETWEEN_TRIGGERS = 5 * 60 * 1000;
+
+  (async () => {
+    for (;;) {
+      console.log("cycling again", new Date().toISOString());
+      plug.setPowerState(false);
+      await delay(TIME_ON_TO_TRIGGER);
+      plug.setPowerState(true);
+      await delay(TIME_BETWEEN_TRIGGERS);
+    }
+  })();
 })();
